e4e1bf99bdc47f4feae382e8a1700c53e8befc72
Amarnadhkumar <102464997+Amarnadhkumar@users.noreply.github.com>
2022-04-27 09:55:22 +0530

    Add files via upload

---
+++ usingmvc/Controllers/FootballController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.netmvc.context;
+
+namespace MvcCrudOperation.Controllers
+{
+    public class FootballController : Controller
+    {
+        // GET: Football
+        aspNetEntities dbobj = new aspNetEntities();
+        public ActionResult Football()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddFootball(FootBall model)
+        {
+            if (ModelState.IsValid)
+            {
+                FootBall footBallLeague = new FootBall();
+         
+                footBallLeague.TeamName1 = model.TeamName1;
+                footBallLeague.TeamName2 = model.TeamName2;
+                footBallLeague.Matchresult = model.Matchresult;
+                footBallLeague.WinningTeam = model.WinningTeam;
+                footBallLeague.Points = model.Points;
+                dbobj.FootBalls.Add(footBallLeague);
+                dbobj.SaveChanges();
+            }
+            ModelState.Clear();
+
+            return View("Football");
+        }
+    }
+}
---
+++ usingmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.netmvc.MvcApplication" Language="C#" %>
