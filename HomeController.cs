e1c25419efd2a10553b747f4346401a2ade1a190
Amarnadhkumar <102464997+Amarnadhkumar@users.noreply.github.com>
2022-05-01 21:17:20 +0530

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Busdetails.Controllers
+namespace MvcCrudOperation.Controllers
 {
     public class HomeController : Controller
     {
